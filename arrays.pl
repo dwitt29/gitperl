b4eebce7df2e4d4fae90b190f269da30b64634e0
dwitt29 <dwitt29@verizon.net>
2016-05-28 14:25:00 -0400

    remove all old locations

--- arrays.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-@names = qw(dave evan jess daniela mike jim joe);
-
-
-$names[@names]=test;
-
-print "hello";
-
--- ex1ch13.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-
-my @a = qw (John mark carol Neil bruce Dave anna Julia robert Ted);
-my @b = sort { lc $a cmp lc $b } @a;
-
-print sort "@b\n";
